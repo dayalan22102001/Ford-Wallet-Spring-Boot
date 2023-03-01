74a129faaa86cb5bc2a2120b0be59829dedf4305
Dayalan <DDAYALA3@ford.com>
2023-03-01 17:28:32 +0530

    Completed

--- src/main/java/com/example/Wallet/WalletServiceImpl.java
+++ src/main/java/com/example/Wallet/WalletServiceImpl.java
@@ -53,17 +53,53 @@ public class WalletServiceImpl implements WalletService{
 
     @Override
     public Double addFunds(Integer id, Double amount) throws WalletException {
-        return null;
+        Optional<WalletDto> walletDtoOptional = this.walletJpaRepository.findById(id);
+        if ( walletDtoOptional.isEmpty()){
+            throw new WalletException("Wallet of id " + id + " could not be found !");
+
+        }
+        WalletDto foundWallet = walletDtoOptional.get();
+        Double newBalance = foundWallet.getBalance() + amount;
+        foundWallet.setBalance(newBalance);
+        this.walletJpaRepository.save(foundWallet);
+        return newBalance;
     }
 
     @Override
     public Double withdrawFunds(Integer id, Double amount) throws WalletException {
-        return null;
+        Optional<WalletDto> walletDtoOptional = this.walletJpaRepository.findById(id);
+        if ( walletDtoOptional.isEmpty()){
+            throw new WalletException("Wallet of id " + id + " could not be found !");
+
+        }
+        WalletDto foundWallet = walletDtoOptional.get();
+        Double newBalance = foundWallet.getBalance() - amount;
+        foundWallet.setBalance(newBalance);
+        this.walletJpaRepository.save(foundWallet);
+        return newBalance;
     }
 
     @Override
     public String tranfersFunds(Integer fromId, Integer toId, Double amount) throws WalletException {
-        return null;
+        Optional<WalletDto> fromWalletDtoOptional = this.walletJpaRepository.findById(fromId);
+        Optional<WalletDto> toWalletDtoOptional = this.walletJpaRepository.findById(toId);
+        if ( fromWalletDtoOptional.isEmpty() || toWalletDtoOptional.isEmpty()){
+            throw new WalletException("Enter valid ID");
+
+        }
+        WalletDto fromWallet =fromWalletDtoOptional.get();
+        WalletDto toWallet = toWalletDtoOptional.get();
+
+        Double fromWalletBalance = fromWallet.getBalance() - amount ;
+        Double toWalletBalance = toWallet.getBalance() + amount;
+
+        fromWallet.setBalance(fromWalletBalance);
+        toWallet.setBalance(toWalletBalance);
+
+        this.walletJpaRepository.save(fromWallet);
+        this.walletJpaRepository.save(toWallet);
+        return "Tranfer Complete" + "\n Balance of : \n "+fromId + " : "+fromWalletBalance
+                +" \n " + toId + " : " + toWalletBalance;
     }
 
     @Override
